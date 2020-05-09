d5ceee1e6e40d2d2a7bb30200ad531abfe12288c
Patrick Kiernan <00decy.pk@gmail.com>
2020-05-09 23:02:41 +0100

    Updated schema.sql to include feats and feat_traits table. Updated pathfinder2.db to include feat data.

--- schema.sql
+++ schema.sql
@@ -4,3 +4,23 @@ CREATE TABLE IF NOT EXISTS traits(
     description TEXT,
     source TEXT
 );
+
+CREATE TABLE IF NOT EXISTS feats(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    action TEXT,
+    level INTEGER,
+    prerequisites TEXT,
+    archetype TEXT,
+    frequencey TEXT,
+    requirements TEXT,
+    trigger TEXT,
+    description TEXT,
+    source TEXT
+);
+
+CREATE TABLE IF NOT EXISTS feat_traits(
+    trait_id INTEGER REFERENCES traits(id),
+    feat_id INTEGER REFERENCES feats(id),
+    trait_value TEXT
+);
